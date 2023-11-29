8a09f3833dfd4fa031d76fadf1ff61a134535caa
Md. Monirul Islam <mmimonir.ctg@gmail.com>
2023-11-30 00:41:25 +0600

    base url edited

--- admin/src/Constants.js
+++ admin/src/Constants.js
@@ -1,5 +1,7 @@
 const Constants = {
-  BASE_URL: "http://localhost:8000/api",
+  // BASE_URL: "http://localhost:8001/api",
+  // BASE_URL: "http://localhost:8000/api",
+  BASE_URL: "https://api.bajajpoint.xyz/api",
 };
 
 export default Constants;
